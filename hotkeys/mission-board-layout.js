2c310b4d8623a5268fb829e5cfe107fcb34da9cd
mrschljakhta-max <mr.schljakhta@gmail.com>
2026-09-04 09:31:12 +0300

    feat: apply approved sector board layout and 11-thread map

--- hotkeys/mission-board-layout.js
+++ hotkeys/mission-board-layout.js
@@ -1,24 +1,27 @@
 window.VIDLIK_MISSION_BOARD = Object.freeze({
   background: 'assets/backgrounds/sectors-board.webp',
   cards: {
-    1: { src: 'assets/cards/sector-01.webp', x: 5.7993265107323895, y: 12.573024810711631, w: 20 },
-    2: { src: 'assets/cards/sector-02.webp', x: 28.56689371472617, y: 15.108066638505667, w: 20 },
-    3: { src: 'assets/cards/sector-03.webp', x: 51.2, y: 11.359528143580453, w: 20 },
-    4: { src: 'assets/cards/sector-04.webp', x: 73.70067093763065, y: 10.564962708449462, w: 20 },
-    5: { src: 'assets/cards/sector-05.webp', x: 8.366217673821593, y: 53.070231391966274, w: 20 },
-    6: { src: 'assets/cards/sector-06.webp', x: 31.5, y: 52.91349213088768, w: 20 },
-    7: { src: 'assets/cards/sector-07.webp', x: 54.33177461656041, y: 49.36496724469296, w: 20 },
-    8: { src: 'assets/cards/sector-08.webp', x: 76.43310628527382, y: 55.74852942243822, w: 20 }
+    1: { src: 'assets/cards/sector-01.webp', x: 6,    y: 14,   w: 20 },
+    2: { src: 'assets/cards/sector-02.webp', x: 28.5, y: 13.8, w: 20 },
+    3: { src: 'assets/cards/sector-03.webp', x: 51.2, y: 13.5, w: 20 },
+    4: { src: 'assets/cards/sector-04.webp', x: 73.5, y: 13.3, w: 20 },
+    5: { src: 'assets/cards/sector-05.webp', x: 8.5,  y: 52,   w: 20 },
+    6: { src: 'assets/cards/sector-06.webp', x: 31.5, y: 52.2, w: 20 },
+    7: { src: 'assets/cards/sector-07.webp', x: 54.8, y: 52.1, w: 20 },
+    8: { src: 'assets/cards/sector-08.webp', x: 76.5, y: 52.3, w: 20 }
   },
   threads: [
-    { from: [1, 'right'], to: [6, 'top'] },
-    { from: [1, 'right'], to: [3, 'left'] },
-    { from: [1, 'right'], to: [5, 'top'] },
-    { from: [3, 'right'], to: [5, 'top'] },
-    { from: [3, 'top'], to: [7, 'top'] },
-    { from: [3, 'right'], to: [8, 'top'] },
-    { from: [4, 'bottom'], to: [7, 'top'] },
-    { from: [4, 'top'], to: [8, 'top'] }
+    { from: [1, 'top'],   to: [5, 'top'] },
+    { from: [2, 'top'],   to: [1, 'top'] },
+    { from: [6, 'top'],   to: [1, 'top'] },
+    { from: [3, 'top'],   to: [6, 'top'] },
+    { from: [3, 'top'],   to: [5, 'top'] },
+    { from: [3, 'top'],   to: [7, 'top'] },
+    { from: [3, 'top'],   to: [1, 'right'] },
+    { from: [4, 'top'],   to: [7, 'top'] },
+    { from: [6, 'right'], to: [4, 'top'] },
+    { from: [4, 'top'],   to: [8, 'top'] },
+    { from: [7, 'right'], to: [8, 'left'] }
   ],
   anchors: {
     top:    { x: 0.50, y: 0.07 },
