c8d370e607eddfb66834a2c9f3b3140ccb0c5f2d
mrschljakhta-max <mr.schljakhta@gmail.com>
2026-09-07 21:41:18 +0300

    Use direct Excel episode assets from Sector 3 page

--- hotkeys/sector3-language-cleanup.js
+++ hotkeys/sector3-language-cleanup.js
@@ -24,23 +24,8 @@ function installExcelChatDedupe(){
   }
  }).observe(chat,{childList:true});
 }
-function loadExcelEpisode(){
- if(!document.querySelector('link[data-vidlik-excel-story]')){
-  const link=document.createElement('link');
-  link.rel='stylesheet';
-  link.href='sector3-excel-tutorial.css?v=20260907-1';
-  link.dataset.vidlikExcelStory='true';
-  document.head.appendChild(link);
- }
- if(window.VIDLIK_EXCEL_STORY_TUTORIAL||document.querySelector('script[data-vidlik-excel-story]'))return;
- const script=document.createElement('script');
- script.src='sector3-excel-tutorial.js?v=20260907-1';
- script.dataset.vidlikExcelStory='true';
- document.head.appendChild(script);
-}
 window.addEventListener('vidlik:language-section-complete',cleanup);
 window.addEventListener('vidlik:os-reset',cleanup);
 cleanup();
 installExcelChatDedupe();
-loadExcelEpisode();
 })();
